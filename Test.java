df358c38a3da54dcdd790e207f75803ded020980
alyamukhbita <alyamukhbita237@gmail.com>
2025-10-02 14:12:38 +0700

    user 1 menambahkan baris

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama test");
+        System.out.println("Hi, Ini baris kode pertama yang di modifikasi user 1");
+        System.out.println("User 1 menambahkan baris baru");
     }
     
 }
